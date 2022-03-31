ccd4b80e537c6299bd16c454f9fbaa9afe7ba095
saikumarm238 <saikumarm64@gmail.com>
2022-03-31 11:16:10 +0530

    v1

---
+++ src/App.js
@@ -0,0 +1,127 @@
+import { Button, Modal, makeStyles, Input } from '@material-ui/core';
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import Post from './Post'
+
+const BASE_URL = 'http://127.0.0.1:8000/'
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    position: 'absolute',
+    width: 400,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3)
+  }
+}))
+
+function App() {
+  const classes = useStyles();
+  
+  const [posts, setPosts] = useState([]);
+  const [openSignIn, setOpenSignIn] = useState(false);
+  const [openSignUp, setOpenSignUp] = useState(false);
+  const [modalStyle, setModalStyle] = useState(getModalStyle)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [authToken, setAuthToken] = useState(null)
+  const [authTokenType, setAuthTokenType] = useState(null);
+  const [userId, setUserId] = useState('');
+  const [email, setEmail] = useState('')
+
+  useEffect(() =>{
+    fetch(BASE_URL + 'post/all')
+      .then(response => {
+        const json = response.json()
+        console.log(json);
+        if (response.ok) {
+          return json
+        }
+        throw response
+      })
+      .then(data =>{
+        const result =data.sort((a, b) => {
+          const t_a = a.timestamp.split(/[-T:]/);
+          const t_b = b.timestamp.split(/[-T:]/);
+          const d_a = new Date(Date.UTC(t_a[0], t_a[1]-1, t_a[2], t_a[3], t_a[4], t_a[5]));
+          const d_b = new Date(Date.UTC(t_b[0], t_b[1]-1, t_b[2], t_b[3], t_b[4], t_b[5]));
+          return d_b - d_a
+        })
+        return result
+      })
+      .then(data =>{
+        setPosts(data)
+      })
+      .catch(error =>{
+        console.log(error);
+        alert(error)
+      })
+  }, [])
+
+  const signIn = (event) => {
+    event?.preventDefault();
+  }
+
+
+  return (
+    <div className='app'>
+
+<Modal
+    open={openSignIn}
+    onClose={() => setOpenSignIn(false)}>
+
+    <div style={modalStyle} className={classes.paper}>
+      <form className="app_signin">
+        <center>
+          <img className="app_headerImage"
+            src="https://i2.wp.com/mrvsdaily.com/wp-content/uploads/2018/02/new-instagram-text-logo.png"
+            alt="Instagram"/>
+        </center>
+        <Input
+          placeholder="username"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)} />
+        <Input
+          placeholder="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} />
+        <Button
+          type="submit"
+          onClick={signIn}>Login</Button>
+      </form>
+    </div>
+    </Modal>
+
+      <div className='app_header'>
+        <img className='app_headerImage' src='https://i2.wp.com/mrvsdaily.com/wp-content/uploads/2018/02/new-instagram-text-logo.png' alt='Instagram'></img>
+        <div>
+          <Button onClick={() => setOpenSignIn(true)}>Login</Button> 
+          <Button onClick={() => setOpenSignUp(true)}>SignUp</Button> 
+        </div>
+      </div>
+    <div className="app_post">
+      {
+        posts.map(post => (
+          <Post post = {post}/>
+        ))
+      }
+    </div>
+    </div>
+  );
+}
+
+export default App;
