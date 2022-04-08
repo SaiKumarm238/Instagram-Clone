32f26d111a36e3d64ad98f71d0c6d5e203989da0
saikumarm238 <saikumarm64@gmail.com>
2022-04-08 19:38:17 +0530

    V-1.1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Button, Modal, makeStyles, Input } from '@material-ui/core';
 import React, {useState, useEffect} from 'react';
 import './App.css';
-import Post from './Post'
+import Post from './Post';
+import ImageUpload from './imageUpload';
 
 const BASE_URL = 'http://127.0.0.1:8000/'
 
@@ -41,6 +42,29 @@ function App() {
   const [userId, setUserId] = useState('');
   const [email, setEmail] = useState('')
 
+  useEffect(() => {
+    setAuthToken(window.localStorage.getItem('authToken'));
+    setAuthTokenType(window.localStorage.getItem('authTokenType'))
+    setUsername(window.localStorage.getItem('username'))
+    setUserId(window.localStorage.getItem('userId'))
+  }, [])
+
+  useEffect(() => {
+    authToken
+      ? window.localStorage.setItem('authToken', authToken)
+      : window.localStorage.removeItem('authToken')
+    authTokenType
+      ? window.localStorage.setItem('authTokenType', authTokenType)
+      : window.localStorage.removeItem('authTokenType')
+    username
+      ? window.localStorage.setItem('username', username)
+      : window.localStorage.removeItem('username')
+    userId
+      ? window.localStorage.setItem('userId', userId)
+      : window.localStorage.removeItem('userId')
+
+  }, [authToken, authTokenType, userId])
+
   useEffect(() =>{
     fetch(BASE_URL + 'post/all')
       .then(response => {
@@ -72,54 +96,183 @@ function App() {
 
   const signIn = (event) => {
     event?.preventDefault();
+  
+
+    let formData = new FormData();
+    formData.append('username', username);
+    formData.append('password', password);
+
+    const requestOptions = {
+      method: 'POST',
+      body: formData
+    }
+
+    fetch(BASE_URL + 'login', requestOptions)
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      }
+      throw response
+    })
+    .then(data => {
+      console.log(data);
+      setAuthToken(data.access_token)
+      setAuthTokenType(data.token_type)
+      setUserId(data.user_id)
+      setUsername(data.username)
+    })
+    .catch(error => {
+      console.log(error);
+      alert(error)
+    })
+
+    setOpenSignIn(false);
   }
+
+  const signOut = (event) => {
+    setAuthToken(null)
+    setAuthTokenType(null)
+    setUserId('')
+    setUsername('')
+  }
+
+  const signUp = (event) => {
+    event?.preventDefault();
+
+    const json_string = JSON.stringify({
+      username: username,
+      email: email,
+      password: password
+    })
+
+    const requestOption = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: json_string
+    }
+
+    fetch(BASE_URL + 'user/', requestOption)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+        throw response
+      })
+      .then(data => {
+        // console.log(data);
+        signIn();
+      })
+      .catch(error => {
+        console.log(error);
+        alert(error);
+      })
+
+    setOpenSignUp(false)
+    
+  }
+
 
 
   return (
     <div className='app'>
 
-<Modal
-    open={openSignIn}
-    onClose={() => setOpenSignIn(false)}>
+    <Modal
+        open={openSignIn}
+        onClose={() => setOpenSignIn(false)}>
 
-    <div style={modalStyle} className={classes.paper}>
-      <form className="app_signin">
-        <center>
-          <img className="app_headerImage"
-            src="https://i2.wp.com/mrvsdaily.com/wp-content/uploads/2018/02/new-instagram-text-logo.png"
-            alt="Instagram"/>
-        </center>
-        <Input
-          placeholder="username"
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)} />
-        <Input
-          placeholder="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)} />
-        <Button
-          type="submit"
-          onClick={signIn}>Login</Button>
-      </form>
-    </div>
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app_signin">
+            <center>
+              <img className="app_headerImage"
+                src="https://i2.wp.com/mrvsdaily.com/wp-content/uploads/2018/02/new-instagram-text-logo.png"
+                alt="Instagram"/>
+            </center>
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)} />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} />
+            <Button
+              type="submit"
+              onClick={signIn}>Login</Button>
+          </form>
+        </div>
+      </Modal>
+
+    <Modal
+      open={openSignUp}
+      onClose={() => setOpenSignUp(false)}>
+
+      <div style={modalStyle} className={classes.paper}>
+        <form className="app_signin">
+          <center>
+            <img className="app_headerImage"
+              src="https://i2.wp.com/mrvsdaily.com/wp-content/uploads/2018/02/new-instagram-text-logo.png"
+              alt="Instagram"/>
+          </center>
+          <Input
+            placeholder="username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)} />
+          <Input
+            placeholder="email"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)} />
+          <Input
+            placeholder="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} />
+          <Button
+            type="submit"
+            onClick={signUp}>Sign up</Button>
+        </form>
+      </div>
+
     </Modal>
+
 
       <div className='app_header'>
         <img className='app_headerImage' src='https://i2.wp.com/mrvsdaily.com/wp-content/uploads/2018/02/new-instagram-text-logo.png' alt='Instagram'></img>
-        <div>
-          <Button onClick={() => setOpenSignIn(true)}>Login</Button> 
-          <Button onClick={() => setOpenSignUp(true)}>SignUp</Button> 
-        </div>
+        {authToken ? (
+            <Button onClick={() => signOut()}>Logout</Button>
+          ) : (
+            <div>
+              <Button onClick={() => setOpenSignIn(true)}>Login</Button> 
+              <Button onClick={() => setOpenSignUp(true)}>SignUp</Button> 
+            </div>
+          )
+        }       
       </div>
     <div className="app_post">
       {
         posts.map(post => (
-          <Post post = {post}/>
+          <Post post = {post}
+          authToken={authToken}
+          authTokenType={authTokenType}
+          username={username}/>
         ))
       }
     </div>
+
+      {
+        authToken ? (
+          <ImageUpload
+          authToken={authToken}
+          authTokenType={authTokenType}
+          userId={userId}
+        />
+        ): (
+          <h3>You need to login to upload</h3>
+        )
+      }
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -5,10 +5,11 @@ import { Avatar, Button } from "@material-ui/core";
 const BASE_URL = 'http://127.0.0.1:8000/'
 
 
-function Post({ post }) {
+function Post({ post, authToken, authTokenType, username }) {
 
     const [imageUrl, setImageUrl] = useState('')
     const [comments, setComments] = useState([])
+    const [newComment, setNewComment] = useState('')
 
     useEffect(() =>{
         if (post.image_url_type == 'absolute') {
@@ -16,7 +17,6 @@ function Post({ post }) {
         } else {
             setImageUrl(BASE_URL + post.image_url)
         }
-    
     }, [])
 
 
@@ -25,7 +25,80 @@ function Post({ post }) {
 
     }, [])
 
+    const handleDelete = (event) => {
+        event?.preventDefault();
 
+        const requestOptions = {
+            method: 'DELETE',
+            headers: new Headers({
+            'Authorization': authTokenType + ' ' + authToken
+            })
+        }
+
+        fetch(BASE_URL + 'post/delete/' + post.id, requestOptions)
+            .then(response => {
+            if (response.ok) {
+                window.location.reload()
+            }
+            throw response
+            })
+            .catch(error => {
+            console.log(error);
+            })
+    }
+
+    const postComment = (event) => {
+        event?.preventDefault()
+
+        const json_string = JSON.stringify({
+            'username': username,
+            'text': newComment,
+            'post_id': post.id
+          })
+
+        const requestOptions = {
+        method: 'POST',
+        headers: new Headers({
+            'Authorization': authTokenType + ' ' + authToken,
+            'Content-Type': 'application/json'
+        }),
+        body: json_string
+        }
+
+        fetch(BASE_URL + 'comment', requestOptions)
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                }
+            })
+            .then(data => {
+                fetchComments()
+            })
+            .catch(error => {
+                console.log(error);
+            })
+            .finally(() => {
+                setNewComment('')
+        })
+    }
+
+    const fetchComments = () => {
+        fetch(BASE_URL + 'comment/all/' + post.id)
+          .then(response => {
+            if (response.ok) {
+              return response.json()
+            }
+            throw response
+          })
+          .then(data => {
+            setComments(data)
+          })
+          .catch(error => {
+            console.log(error);
+          })
+    }  
+    
+        
     return (
         <div className="post">
             <div className="post_header">
@@ -33,7 +106,7 @@ function Post({ post }) {
                 src=""/>
                 <div className="post_headerInfo">
                     <h3>{post.user.username}</h3>
-                    <Button className="post_delete">Delete</Button>
+                    <Button className="post_delete" onClick={handleDelete}>Delete</Button>
                 </div>
             </div>
 
@@ -47,11 +120,27 @@ function Post({ post }) {
                         <p>
                             <strong>{comments.username}:</strong> {comments.text}
                         </p>
-
                     ))
-                    
                 }
             </div>
+
+            {authToken && (
+                <form className="post_commentbox">
+                    <input className="post_input"
+                        type="text"
+                        placeholder="Add a comment"
+                        value={newComment}
+                        onChange={(e) => setNewComment(e.target.value)}
+                    />
+                    <button
+                        className="post_button"
+                        type="submit"
+                        disabled={!newComment}
+                        onClick={postComment}>
+                        Post
+                        </button>
+                </form>
+            )}
         </div>
     )
 }
